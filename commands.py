aa1ad33b5173991d742106eb9fa3a3f8df802f0f
Ayush Gupta <ayushkgupta007@gmail.com>
2019-10-13 02:29:14 +0530

    processing input and initialized command's methods

---
+++ commands.py
@@ -0,0 +1,22 @@
+def park_new_car(parking_lot, registration_no, colour):
+    print(1)
+
+
+def car_departure(parking_lot, slot_number):
+    print(1)
+
+
+def get_status(parking_lot):
+    print(1)
+
+
+def get_registration_by_colour(parking_lot, colour):
+    print(1)
+
+
+def get_slot_by_colour(parking_lot, colour):
+    print(1)
+
+
+def get_slot_by_registration(parking_lot, registration_no):
+    print(1)
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import sys
+from classes.parking_lot import ParkingLot
+from commands import *
 
 
-def execute_command(command):
-    print(command)
+def execute_command(parking_lot, command):
+    if command[0] == "create_parking_lot":
+        parking_lot = ParkingLot(command[1])
+    elif command[0] == "park":
+        park_new_car(parking_lot, command[1], command[2])
+    elif command[0] == "leave":
+        car_departure(parking_lot, command[1])
+    elif command[0] == "status":
+        get_status(parking_lot)
+    elif command[0] == "registration_numbers_for_cars_with_colour":
+        get_registration_by_colour(parking_lot, command[1])
+    elif command[0] == "slot_numbers_for_cars_with_colour":
+        get_slot_by_colour(parking_lot, command[1])
+    elif command[0] == "slot_number_for_registration_number":
+        get_slot_by_registration(parking_lot, command[1])
+    else:
+        print('No such command')
 
 
 def interactive_mode():
+    parking_lot = None
     command = input().split()
     while not command[0] == 'exit':
-        execute_command(command)
+        execute_command(parking_lot, command)
         command = input().split()
 
 
 def file_read_mode(file_name):
+    parking_lot = None
     with open(file_name) as file:
         commands = file.readlines()
         for command in commands:
             command.replace('\n', '').split()
-            execute_command(command)
+            execute_command(parking_lot, command)
 
 
 def main():
